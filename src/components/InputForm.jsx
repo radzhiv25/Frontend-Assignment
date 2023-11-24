0d3fe9b31ada96d84b87f6878479ac49c3004c91
radzhiv <radzhivkrishna@gmail.com>
2023-11-24 21:28:18 +0530

    Feat: Input and Preview form created

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+export const InputForm = () => {
+  return (
+    <div className="text-center font-bold">
+        <h1>Input Form</h1>
+    </div>
+  )
+}
---
+++ src/components/PreviewForm.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+export const PreviewForm = () => {
+  return (
+    <div className="text-center font-bold">
+        <h1>Preview Form</h1>
+    </div>
+  )
+}
