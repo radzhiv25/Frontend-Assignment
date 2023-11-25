d9edc9bf43d75e145f37ca65070561b659feb02a
radzhiv <radzhivkrishna@gmail.com>
2023-11-25 13:16:17 +0530

    Feat: Components updated

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,9 +1,32 @@
 // import React from 'react'
+import { useState } from "react"
 
 export const InputForm = () => {
+  const [input, setInput] = useState('')
+  const [output, setOutput] = useState('')
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleChange = (e) => {
+    setInput(e.target.value)
+    console.log(JSON.parse(input))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }
+
   return (
-    <div className="text-center font-bold">
-        <h1>Input Form</h1>
+    <div className="text-center p-2">
+        <h1 className="font-bold">JSON Preview</h1>
+        <textarea name="" id="" rows='35'  className="border w-full rounded resize-none">
+          {input}
+        </textarea>
+        <button className="text-white py-2 px-3 bg-black rounded" onClick={handleChange}>Submit</button>
     </div>
   )
 }
--- src/components/PreviewForm.jsx
+++ src/components/PreviewForm.jsx
@@ -4,6 +4,9 @@ export const PreviewForm = () => {
   return (
     <div className="text-center font-bold">
         <h1>Preview Form</h1>
+        <form action="">
+          <label htmlFor="">{pizza_name}</label>
+        </form>
     </div>
   )
 }
