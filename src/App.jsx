9b6c7ebc661615e420b6f2d249652c760284775a
radzhiv <radzhivkrishna@gmail.com>
2023-11-26 23:08:18 +0530

    Feat: Merged InputForm in App and made minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,44 @@
-// import { useState } from 'react'
-
-import { InputForm } from "./components/InputForm";
+import { useState } from "react";
 import { PreviewForm } from "./components/PreviewForm";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [data, setData] = useState("");
+  const [preview, setPreview] = useState("");
 
+  const onChangeData = (e) => {
+    setData(e.target.value);
+    // console.log(data)
+  };
+
+  const onSubmit = () => {
+    setPreview(data);
+    console.log(data);
+  };
   return (
-    <div className="m-5">
-      <h1 className="text-center text-3xl font-bold text-gray-500">
-        preview schema
-      </h1>
-      <div className="flex md:flex-row flex-col my-3 gap-5">
-        <div className="md:w-1/2 border md:h-screen rounded">
-          <InputForm />
-        </div>
-        <div className="md:w-1/2 border md:h-screen rounded">
-          <PreviewForm />
-        </div>
+    <div className="flex">
+      <div className="m-2 p-3 border-2 md:w-1/2 rounded shadow-lg">
+        <h1 className="text-2xl font-bold text-center">JSON Text Editor</h1>
+        <textarea
+          value={data}
+          name=""
+          id=""
+          cols="30"
+          rows="20"
+          onChange={onChangeData}
+          className="border resize-none rounded w-full"
+        >
+          {data}
+        </textarea>
+        <button
+          className="text-white bg-black p-2 rounded block mx-auto"
+          onClick={onSubmit}
+        >
+          Submit
+        </button>
+      </div>
+      <div className="border-2 w-1/2 m-2 p-2 shadow-lg">
+        <h1 className="text-center font-bold text-2xl">Preview Form</h1>
+        <PreviewForm preview={preview} />
       </div>
     </div>
   );
